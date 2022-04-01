d26856e9eb2868d7284edfeb2c29fabc0f25f976
ChaunceyKong <45418162+ChaunceyKong@users.noreply.github.com>
2022-04-01 19:54:53 +0800

    Add files via upload

---
+++ array/removeElement.java
@@ -0,0 +1,26 @@
+
+public class Test {
+    public static void main(String[] args) {
+        int[] nums = {3,2,2,3};
+        Solution solution = new Solution();
+        System.out.println(solution.removeElement(nums, 3));
+    }
+}
+
+class Solution {
+    //思路：双指针
+    //1.右指针r依次遍历，搜索非val值
+    //2.若nums[r]!=val，则nums[l]==nums[r]，左指针l++；否则跳过nums[r]
+    public int removeElement(int[] nums, int val) {
+        int len = 0;
+
+        for (int r = 0, l = 0; r < nums.length; r++) {
+            if (nums[r] != val) {
+                nums[l] = nums[r];
+                l++;
+            }
+            len = l;
+        }
+        return len;
+    }
+}
